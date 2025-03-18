615ab2f7177a65a28749feda2475ac03ff3b06f9
DNK628 <bonnsfag@gmail.com>
2025-03-18 21:25:06 +0200

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -5,43 +5,26 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-//Модифікована програма регістрації
-
-namespace Forms_lab_1
+namespace WF_LAB3
 {
     public partial class Form1 : Form
     {
+        const decimal LightBeerPrice = 30;
+        const decimal DarkBeerPrice = 35;
+        const decimal DiscountThreshold = 20;
+        const decimal DiscountRate = 0.15m;
+
         public Form1()
         {
             InitializeComponent();
-            this.KeyPreview = true; 
-            this.KeyDown += Form1_KeyDown;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
-            this.KeyPreview = true;
-            this.KeyDown += new KeyEventHandler(Form1_Load);
 
-        }
-        private void Form1_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Enter)
-            {
-                button1.PerformClick();
-            }
-            else if (e.KeyCode == Keys.Escape)
-            {
-                button2.PerformClick(); 
-            }
-        }
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -49,56 +32,56 @@ namespace Forms_lab_1
 
         }
 
-        private void label4_Click(object sender, EventArgs e)
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            numericUpDown1.Value = 0;
+            numericUpDown2.Value = 0;
+            textBox3.Clear();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string firstName = textBox1.Text.Trim();
-            string lastName = textBox2.Text.Trim();
-            string email = textBox4.Text.Trim();
+            int lightBeerCount = (int)numericUpDown1.Value;
+            int darkBeerCount = (int)numericUpDown2.Value;
+            int totalBeers = lightBeerCount + darkBeerCount;
 
-            int birthYear = (int)numericUpDown3.Value;
-            int birthMonth = (int)numericUpDown2.Value;
-            int birthDay = (int)numericUpDown1.Value;
+            decimal totalCost = (lightBeerCount * LightBeerPrice) + (darkBeerCount * DarkBeerPrice);
 
-            if (birthYear < 1900 || birthYear > DateTime.Now.Year)
+            if (totalBeers > DiscountThreshold)
             {
-                MessageBox.Show("Некоректний рік народження.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
+                totalCost -= totalCost * DiscountRate;
             }
 
-            if (birthMonth < 1 || birthMonth > 12)
-            {
-                MessageBox.Show("Некоректний місяць.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-
-            if (birthDay < 1 || birthDay > 31)
-            {
-                MessageBox.Show("Некоректний день.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-
-            string gender = radioButton1.Checked ? "Чоловік" : radioButton2.Checked ? "Жінка" : "Не вказано";
-
-            string result = $"Ім’я: {firstName}\nПрізвище: {lastName}\nРік народження: {birthYear}\nМісяць: {birthMonth}\nДень: {birthDay}\nEmail: {email}\nСтать: {gender}";
-            textBox5.Text = result;
+            textBox3.Text = totalCost.ToString("0.00");
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
         {
 
         }
@@ -108,22 +91,7 @@ namespace Forms_lab_1
 
         }
 
-        private void label7_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void numericUpDown2_ValueChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void radioButton1_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox5_TextChanged(object sender, EventArgs e)
         {
 
         }
